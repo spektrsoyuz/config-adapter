0025bb1dcc844fdd4dbb73acfec1259580b2c4e9
SpektrSoyuz <45124883+spektrsoyuz@users.noreply.github.com>
2026-07-27 19:58:01 -0400

    Add GitHub workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
     id("io.freefair.lombok") version "9.5.0"
 }
 
@@ -18,14 +19,30 @@ dependencies {
     implementation("org.spongepowered:configurate-hocon:4.3.0-SNAPSHOT")
 
     compileOnly("io.papermc.paper:paper-api:26.2.build.+")
-
-    testImplementation("org.junit.jupiter:junit-jupiter:6.1.1")
 }
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
 
-tasks.test {
-    useJUnitPlatform()
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            artifactId = "config-adapter"
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/spektrsoyuz/config-adapter")
+
+            credentials {
+                System.getenv("REPOSITORY_USER")
+                System.getenv("REPOSITORY_TOKEN")
+            }
+        }
+    }
 }
