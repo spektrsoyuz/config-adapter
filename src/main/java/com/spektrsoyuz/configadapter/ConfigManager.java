c9d7b0bc7fcb28a51e8a741ef6ba61b91045794a
SpektrSoyuz <45124883+spektrsoyuz@users.noreply.github.com>
2026-07-27 19:35:45 -0400

    Add ConfigManager class

---
+++ src/main/java/com/spektrsoyuz/configadapter/ConfigManager.java
@@ -0,0 +1,118 @@
+package com.spektrsoyuz.configadapter;
+
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.spongepowered.configurate.serialize.SerializationException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Manages configuration files.
+ *
+ * @since 1.0.0
+ */
+public final class ConfigManager {
+
+    private final JavaPlugin plugin;
+    private final ComponentLogger logger;
+    private final Map<String, ConfigWrapper> configs;
+
+    // Constructor
+    public ConfigManager(final JavaPlugin plugin) {
+        this.plugin = plugin;
+        this.logger = plugin.getComponentLogger();
+        this.configs = new ConcurrentHashMap<>();
+    }
+
+    // Adds a config to the manager
+    public void addConfig(final String fileName) {
+        this.configs.put(fileName, new ConfigWrapper(this.plugin, fileName));
+    }
+
+    // Loads all configs
+    public void load() {
+        for (final ConfigWrapper config : this.configs.values()) {
+            config.load();
+        }
+    }
+
+    // Saves all configs
+    public void save() {
+        for (final ConfigWrapper config : this.configs.values()) {
+            config.save();
+        }
+    }
+
+    // Checks if all configs are loaded
+    public boolean isLoaded() {
+        for (final ConfigWrapper config : this.configs.values()) {
+            if (config.getNode() == null) return false;
+        }
+        return true;
+    }
+
+    // Returns a value from a config
+    public <T> T getConfig(
+            final String configKey,
+            final String nodeName,
+            final Class<T> clazz,
+            final T defaultInstance
+    ) {
+        final ConfigWrapper wrapper = this.configs.get(configKey);
+        if (wrapper == null) return defaultInstance;
+
+        try {
+            final T config = wrapper.getNode().node(nodeName).get(clazz);
+
+            return config != null
+                    ? config
+                    : defaultInstance;
+        } catch (final SerializationException e) {
+            this.logger.error("Failed to load '{}' config from '{}', using default values", nodeName, configKey, e);
+            return defaultInstance;
+        }
+    }
+
+    // Returns a list of values from a config
+    public <T> List<T> getList(
+            final String configKey,
+            final String nodeName,
+            final Class<T> clazz
+    ) {
+        final ConfigWrapper wrapper = this.configs.get(configKey);
+        if (wrapper == null) return Collections.emptyList();
+
+        try {
+            final List<T> list = wrapper.getNode().node(nodeName).getList(clazz);
+
+            return list != null
+                    ? list
+                    : Collections.emptyList();
+        } catch (final SerializationException e) {
+            this.logger.error("Failed to load list '{}' from '{}'", nodeName, configKey, e);
+            return Collections.emptyList();
+        }
+    }
+
+    // Saves a value to a config
+    public <T> void saveConfig(
+            final String configKey,
+            final String nodeName,
+            final Class<T> clazz,
+            final T instance
+    ) {
+        final ConfigWrapper wrapper = this.configs.get(configKey);
+        if (wrapper == null) return;
+
+        try {
+            wrapper.getNode().node(nodeName).set(clazz, instance);
+            wrapper.save();
+        } catch (final SerializationException e) {
+            this.logger.error("Failed to save '{}' config to '{}'", nodeName, configKey, e);
+        }
+    }
+
+}
